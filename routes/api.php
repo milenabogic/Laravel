7792d55a3423ffbb73964766e34e14f84a044714
milenabogic <mbogic1995@gmail.com>
2021-04-19 10:01:36 +0200

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ClientsController;
+use App\Http\Controllers\API\EmployeesController;
+use App\Http\Controllers\API\ProjectsController;
+use App\Http\Controllers\API\TimeSheetController;
+use App\Http\Controllers\API\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +20,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+     
+Route::middleware('auth:api')->group( function () {
+    Route::resource('employees', CreateEmployeesTableController::class);
+    Route::resource('clients', CreateClientsTableController::class);
+    Route::resource('time_sheets',  CreateTimeSheetsTableController::class); 
+    Route::resource('projects',  CreateProjectsTableController::class);
 });
